8a9ae41d8c4051e74b02077ae13907d1bd2dc68a
Ralph Jay Pepito <rjpepito24@gmail.com>
2024-01-24 22:13:33 +0800

    Updates

--- plugins/repository.js
+++ plugins/repository.js
@@ -1,6 +1,5 @@
 import { repository } from "../repository/Repository"
 
 export default (context, inject) => {
-  // Inject $hello(msg) in Vue, context and store.
   inject('repository', repository())
 }
